dff4d8f63838d54aac26f55f20d1e1460df63bb0
llxxll12345 <charlieliu@lawn-128-61-41-84.lawn.gatech.edu>
2019-09-23 22:15:21 -0400

    find preliminary

--- server/server.js
+++ server/server.js
@@ -21,34 +21,61 @@ mongoose.connect(mongoDB, { useNewUrlParser: true }, err => {
 
     // a new user object
     const newUser = new UserInfo({
-        createAt: '2019-09-15',
-        userName: 'Charlie',
-        password: passCode,
-        email: 'someName@gatech.edu'
+        accountInfo: {
+            createdAt: '2019-09-23',
+            userName: "asdf",
+            password: passCode
+        }
     });
 
     // sample save (insert), also can use create or insertMany API
-    newUser.save((err) => {
+    /*newUser.save((err) => {
         if (err) 
             throw err;
         console.log('UserInfo stored!' + newUser._id); // This is the way to get the userId
     });
 
     // querying
-    UserInfo.find({ "userName" : 'Charlie' }, (err, userInfo) => {
+    /*UserInfo.find({ "userName" : 'Charlie' }, (err, userInfo) => {
         if (err) 
             throw err;
         console.log("Userinfo ", userInfo);
-    })
+    });*/
+
+    console.log({"accountInfo.userName" : "asdf"});
+    UserInfo.find({"accountInfo.userName" : "asdf"})
+            .then(docs => {
+                console.log(docs);
+                for (let item of docs){
+                    console.log(item.get("accountInfo").userName);
+                } 
+            });
 });
 
-
-app.post('/getapi', (req, res) => {
-   
+app.post('/findpost', (req, res) => {
+    const key = String(req.body.key);
+    const value = String(req.body.value);
+    queryFromDb(key, value, res);
 });
 
-app.get('/postapi', (req, res) => {
-
+app.get('/generalFindGet', (req, res) => {
+    const key = String(req.query.key);
+    const value = String(req.query.value);
+    queryFromDb(key, value, res);
 });
+
+function queryFromDb(key, value, res) {
+    console.log("req: " + key + " " + value + " " + (key ==="accountInfo.userName"));
+    obj = {}
+    obj[key] = value
+    console.log(obj)
+    UserInfo.find(obj)
+            .then(docs => {
+                console.log(docs);
+                res.send(docs);
+            })
+            .catch(err => console.log(err));
+}
+
 
 app.listen(port, () => console.log('Server started at port 5000'))
