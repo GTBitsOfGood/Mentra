be9fe7cdf225fbb211e40577f466df5615c5a087
Sladuca <sladuca777@gmail.com>
2019-10-30 12:05:30 -0400

    add mutation and input types

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -2,15 +2,65 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
     type Query {
-      userInfo(fullName: String!): UserInfo
+      userInfo(fullName: String!): UserInfo!
     }
+    type Mutation {
+      createUser(userInfo: UserInfoInput!): UserInfo!
+      updateUser(userInfo: UserInfoInput!): UserInfo!
+    }
+    # INPUT TYPES
+
+    input UserInfoInput {
+      accountInfo: AccountInfoInput!
+      identifyingInfo: IdentifyingInfoInput
+      experienceInfo: ExperienceInfoInput
+      workPreferenceInfo: WorkPreferenceInfoInput
+      disabilityInfo: DisabilityInfoInput
+    }
+
+    input IdentifyingInfoInput {
+      fullName: String!
+      email: String!
+      phoneNumber: String
+      gender: Gender
+      ethnicity: Ethnicity
+      race: Race
+      address: Address
+      age: Int
+    }
+
+    input ExperienceInfoInput {
+      training: [vocationalTrainingInfo!]!
+      education: [educationalInfo!]!
+      legalAuthorization: Boolean
+      sponsorship: Boolean
+      resume: String
+    }
+
+    input WorkPreferenceInfoInput {
+      timing: timingInfo!
+      workingSpace: spaceInfo!
+      tasks: tasksInfo!
+      situation: situationInfo!
+      flexibility: flexibilityInfo!
+      teamwork: Boolean!
+    }
+
+    input DisabilityInfoInput {
+      employable: Boolean!
+      speechAbility: Boolean!
+      createdOwnProfile: Boolean!
+      diagnosis: Boolean!
+    }
+
     # TOP LEVEL AGGREGATES
     type UserInfo {
+      id: ID!
       accountInfo: AccountInfo!
-      identifyingInfo: identifyingInfo!
-      experienceInfo: ExperienceInfo!
-      workPreferenceInfo: WorkPreferenceInfo!
-      disabilityInfo: DisabilityInfo!
+      identifyingInfo: identifyingInfo
+      experienceInfo: ExperienceInfo
+      workPreferenceInfo: WorkPreferenceInfo
+      disabilityInfo: DisabilityInfo
     }
     # 2nd LEVEL AGGREGATES
     type AccountInfo {
