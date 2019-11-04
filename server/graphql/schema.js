3580076bb86f7524fdbc77083c82b1b32d9bdd92
Sladuca <sladuca777@gmail.com>
2019-11-03 20:22:57 -0500

    make required input types

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -28,27 +28,27 @@ const typeDefs = gql`
       fullName: String!
       email: String!
       phoneNumber: String
-      gender: Gender
-      ethnicity: Ethnicity
-      race: Race
-      address: Address
+      gender: GenderInput
+      ethnicity: EthnicityInput
+      race: RaceInput
+      address: AddressInput
       age: Int
     }
 
     input ExperienceInput {
-      training: [Traning!]!
-      education: [Education!]!
+      training: [TraningInput!]!
+      education: [Educationinput!]!
       legalAuthorization: Boolean
       sponsorship: Boolean
       resume: String
     }
 
     input WorkPreferenceInput {
-      timing: Timing!
-      workingSpace: Space!
-      tasks: Tasks!
-      situation: Situation!
-      flexibility: Flexibility!
+      timing: TimingInput!
+      workingSpace: SpacesInput!
+      tasks: TasksInput!
+      situation: SituationInput!
+      flexibility: FlexibilityInput!
       teamwork: Boolean!
     }
 
@@ -63,6 +63,72 @@ const typeDefs = gql`
       university: String!
       degree: Degree!
       graduationYear: Int
+    }
+
+    type vocationalTrainingInput {
+      name: String!
+      coach: String!
+      receivedEducation: Boolean!
+    }
+
+    type EducationInput {
+      university: String!
+      degree: Degree!
+      graduationYear: Int
+    }
+
+    type TimingInput {
+      changingHours: Boolean!
+      earlyMorning: Boolean!
+      standardHours: Boolean!
+      lateNights: Boolean!
+      weekends: Boolean!
+    }
+
+    type SpacesInput {
+      noisyEnvironment: Boolean!
+      brightLights: Boolean!
+      openFoodArea: Boolean!
+      indoorWork: Boolean!
+      outdoorWork: Boolean!
+      uniformWork: Boolean!
+    }
+
+    type TasksInput {
+      dataEntry: Boolean!
+      drivingTasks: Boolean!
+      periodStanding: Boolean!
+      socialInteraction: Boolean!
+      heavyLifting: Boolean!
+      workWithAnimals: Boolean!
+    }
+
+    type SituationsInput {
+      manyTasks: Boolean!
+      tightdeadlines: Boolean!
+      longWorkPeriods: Boolean!
+      workOnTeams: Boolean!
+      workAlone: Boolean!
+      acceptFeedback: Boolean!
+      changeTasks: Boolean!
+    }
+
+    type FlexibilityInput {
+      manyTasks: Boolean!
+      tightdeadlines: Boolean!
+      longWorkPeriods: Boolean!
+      workOnTeams: Boolean!
+      workAlone: Boolean!
+      acceptFeedback: Boolean!
+      changeTasks: Boolean!
+    }
+
+    type AddressInput {
+      country: String!
+      stateProvince: String!
+      city: String!
+      streetAddress: String!
+      postalCode: String!
     }
 
     # TOP LEVEL AGGREGATES
@@ -189,15 +255,13 @@ const typeDefs = gql`
       # not sure what types we want to include
     }
     enum Ethnicity {
-      LATINX
-      NOT_LATINX
-    }
-    enum Race {
       AMERICAN_INDIAN
       ASIAN
       AFRICAN_AMERICAN
+      LATINX
       PACIFIC_ISLANDER
       WHITE
+      MULTIRACIAL
     }
     enum Degree {
       ASSOCIATE
