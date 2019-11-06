94cc1527fc4984cdc45d6c40a958c269946f7d06
Sladuca <sladuca777@gmail.com>
2019-11-06 10:22:12 -0500

    put in localhost for Lixing's mongo connector

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,7 +1,7 @@
 const { ApolloServer, makeExecutableSchema } = require('apollo-server')
 const { typeDefs } = require('./schema')
 
-const MONGO_CONNECTOR_URL = 'http://name.domain'
+const MONGO_CONNECTOR_URL = 'http://localhost:4001'
 
 
 const resolvers = {}
