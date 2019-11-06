c73b645ef8301aeb3045790cf969348cde1464f3
Sladuca <sladuca777@gmail.com>
2019-11-06 10:54:44 -0500

    spiff up server messages

--- server/onboarding/server.js
+++ server/onboarding/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', routes);
 
-const mongoDB = 'mongodb://localhost:27017/mentra';
+const mongoDB = 'mongodb://0.0.0.0:27017/mentra';
 mongoose.connect(mongoDB, { useNewUrlParser: true }, err => {
     if (err)
         throw err;
@@ -27,4 +27,4 @@ app.get('/deleteGet', DeleteController.deleteGet);
 app.get('/updateGet', UpdateController.updateGet);
 */
 
-app.listen(port, () => console.log('Server started at port 5000'))
+app.listen(port, () => console.log(`📗 Mongo Connector ready on port ${port}`))
