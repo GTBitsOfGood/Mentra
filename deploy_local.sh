c41b79cb3dbf0ba2e1957a28fd44e388b2576f4e
Sebastien La Duca <sladuca777@gmail.com>
2019-11-19 13:00:10 -0500

    update to Lixing's name

--- deploy_local.sh
+++ deploy_local.sh
@@ -3,9 +3,9 @@
 # install node stuff for both backends
 [ -z $1 ] && sh ./deploy_mongo_local.sh
 cd server/graphql && yarn install
-cd ../onboarding && npm install
+cd ../serverImpl && npm install
 cd ../..
 # run Lixing's backend on port 5000
-node server/onboarding/server.js &
+node server/serverImpl/server.js &
 # run Apollo GraphQL Server on port 4000
 node server/graphql/index.js
