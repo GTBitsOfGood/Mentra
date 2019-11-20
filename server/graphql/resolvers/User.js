9132706887a1a1e74bb8cf5064644590e488b869
Sebastien La Duca <sladuca777@gmail.com>
2019-11-19 20:32:55 -0500

    resolver for workpreference

--- server/graphql/resolvers/User.js
+++ server/graphql/resolvers/User.js
@@ -6,7 +6,12 @@ async function id(parent, args, context, info) {
   return parent._id
 }
 
+async function workPreference(parent, args, context, info) {
+  return parent.workPreference
+}
+
 module.exports = {
   account,
   id,
+  workPreference,
 }
