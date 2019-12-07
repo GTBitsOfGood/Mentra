315346109032d2f6b1cf758188222dc7b39dc13f
Sebastien La Duca <sladuca777@gmail.com>
2019-12-06 23:13:42 -0500

    move data fetching into it's own module

---
+++ server/models/Candidate.js
@@ -0,0 +1,10 @@
+export default {
+  createCandiate(mongoClient, candidate) {
+    // open connection to Candidate collection
+    // perform write and return result
+  }
+  deleteCandidate(mongoClient, candidateId) {
+    // open connection to candidate collection
+    // perform the delete and return the result
+  }
+}
